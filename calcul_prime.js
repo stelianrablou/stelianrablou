61a7a44730504c64304578f90ceb73706346b254
stelianrablou <44159828+stelianrablou@users.noreply.github.com>
2018-10-29 11:32:53 +0200

    Update calcul_prime.js

--- calcul_prime.js
+++ calcul_prime.js
@@ -1,3 +1,7 @@
+onmessage() = function(e){
+	if (e.data == "stop"){
+		stopped = true;
+}
 //-----------------------------------------
 function is_prim(n)
 {
@@ -7,9 +11,21 @@ function is_prim(n)
 		return true;
 }
 //-----------------------------------------
-for (var i = 1e9; i 1e10; i++)
-	if(is_prim(i))
-		postMessage(i);
-	
-postMessage("gata");
+function calculeaza_prime(start_value)
+{
+	if(stopped)
+		   ;
+	   else{
+			for (var i = start_value; i < start_value + 1000; i++){
+				if(is_prim(i))
+					postMessage(i);
+			}
+			start+=1000;
+		}
+		if (start_value < 1e10)
+			setTimeout(calculeaza_prime, 1, start_value);
+		else
+			postMessage("gata");
+	}
 //-----------------------------------------
+calculeaza_prime(1e9);
