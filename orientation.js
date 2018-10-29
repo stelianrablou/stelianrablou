b519a5fea69f1e2f2209d8cda3ff136032cf4b77
stelianrablou <44159828+stelianrablou@users.noreply.github.com>
2018-10-29 12:51:59 +0200

    Update orientation.js

--- orientation.js
+++ orientation.js
@@ -2,9 +2,21 @@ document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 
 
 window.addEventListener("deviceorientation", on_device_orientation);
 
+//-----------------------------------------------------------
 function on_device_orientation(e)
 {
 		document.getElementById("id_alpha").innerHTML = e.alpha;
 		document.getElementById("id_beta").innerHTML = e.beta;
 		document.getElementById("id_gamma").innerHTML = e.gamma;
+		
+		var canvas = document.getElementById("id_canvas");
+		var context = canvas.getContext("2d"):
+		
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		
+		context.beginPath();
+		context.arc(canvas.width / 2 + e.gamma / 90 * (canvas.width / 2 - R),
+					canvas.height / 2 + e.beta / 90 * (canvas.height / 2 - R),
+					10, 0, 2 * Math.PI);
+		context.stroke();
 }
