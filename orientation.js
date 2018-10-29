ab5640892c47c807eda361c25f2f61598687b6f2
stelianrablou <44159828+stelianrablou@users.noreply.github.com>
2018-10-29 12:23:41 +0200

    Add files via upload

---
+++ orientation.js
@@ -0,0 +1,10 @@
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.10.29.0";
+
+window.addEventListener("deviceorientation", on_device_orientation);
+
+function on_device_orientation(e)
+{
+		document.getElementById("id_alpha").innerHTML = e.alpha;
+		document.getElementById("id_beta").innerHTML = e.beta;
+		document.getElementById("id_gama").innerHTML = e.gama;
+}
