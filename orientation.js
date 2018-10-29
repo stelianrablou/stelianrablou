a7012ce5798702c9d732949dc50b959229bb92db
stelianrablou <44159828+stelianrablou@users.noreply.github.com>
2018-10-29 12:33:21 +0200

    Update orientation.js

--- orientation.js
+++ orientation.js
@@ -6,5 +6,5 @@ function on_device_orientation(e)
 {
 		document.getElementById("id_alpha").innerHTML = e.alpha;
 		document.getElementById("id_beta").innerHTML = e.beta;
-		document.getElementById("id_gama").innerHTML = e.gama;
+		document.getElementById("id_gamma").innerHTML = e.gamma;
 }
